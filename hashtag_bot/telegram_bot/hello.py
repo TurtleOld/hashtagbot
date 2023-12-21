61ed6e3a4a450456275634c125b53014af26fbf8
TurtleOld <dev@pavlovteam.ru>
2023-12-21 07:26:13 +0300

    refactoring

--- hashtag_bot/telegram_bot/hello.py
+++ hashtag_bot/telegram_bot/hello.py
@@ -1,9 +1,12 @@
 import asyncio
 
+from sqlalchemy import select
 from telebot import types
 
 from hashtag_bot.config.logger import logger
 from hashtag_bot.config.bot import bot
+from hashtag_bot.models.database import Session
+from hashtag_bot.models.telegram_message import TelegramMessage
 
 
 @logger.catch
@@ -21,7 +24,35 @@ async def get_message_id(message: types.Message) -> None:
         if mess.startswith('#'):
             hashtag_list_message.append(mess)
     await bot.send_message(message.chat.id, ', '.join(hashtag_list_message))
-    print(message)
+    session: Session = Session()
+    select_message_id = select(TelegramMessage.message_id)
+    message_id = (
+        session.query(TelegramMessage)
+        .filter(TelegramMessage.message_id == int(message.message_id))
+        .scalar_subquery()
+    )
+    # message_id_text = ''.join(list(message_id))
+    session.close()
+    print(message.message_id)
+    if not list(message_id):
+        session: Session = Session()
+        insert_message = TelegramMessage(message_id=message.message_id)
+        session.add(insert_message)
+        session.commit()
+        session.close()
+
+    await bot.edit_message_text(
+        chat_id=message.chat.id,
+        message_id=message_id,
+        text=', '.join(hashtag_list_message),
+    )
+
+    await bot.unpin_all_chat_messages(chat_id=message.chat.id)
+    await bot.pin_chat_message(
+        chat_id=message.chat.id,
+        message_id=select_message_id,
+        disable_notification=True,
+    )
 
 
 def start_bot() -> None:
