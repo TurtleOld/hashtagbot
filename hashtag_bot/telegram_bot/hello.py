dad82379d8c1f0052e2a731db4c5b8ea658cfb5c
TurtleOld <dev@pavlovteam.ru>
2023-12-21 22:52:24 +0300

    refactoring

--- hashtag_bot/telegram_bot/hello.py
+++ hashtag_bot/telegram_bot/hello.py
@@ -27,11 +27,11 @@ def record_hashtags_database(message):
                     session.add(insert_hashtag)
                     session.commit()
 
-    
+
 @logger.catch
+@bot.channel_post_handler(func=lambda message: message.text)
 @bot.message_handler(func=lambda message: message.text)
 async def get_message_id(message: types.Message) -> None:
-    hashtag_list_message = []
     record_hashtags_database(message)
     with Session() as session:
         select_hashtags = select(HashTag.name).distinct()
@@ -39,8 +39,12 @@ async def get_message_id(message: types.Message) -> None:
         select_message_id = select(TelegramMessage.message_id)
         message_ids = session.execute(select_message_id).first()
         if not message_ids:
-            sent_hashtags = await bot.send_message(message.chat.id, ", ".join([tag[0] for tag in hashtags]))
-            insert_message_id = TelegramMessage(message_id=sent_hashtags.message_id)
+            sent_hashtags = await bot.send_message(
+                message.chat.id, ", ".join([tag[0] for tag in hashtags])
+            )
+            insert_message_id = TelegramMessage(
+                message_id=sent_hashtags.message_id
+            )
             session.add(insert_message_id)
             session.commit()
             await bot.pin_chat_message(
@@ -48,42 +52,12 @@ async def get_message_id(message: types.Message) -> None:
                 message_id=sent_hashtags.message_id,
                 disable_notification=True,
             )
-        
+
         await bot.edit_message_text(
             chat_id=message.chat.id,
             message_id=int(message_ids[0]),
             text=", ".join([tag[0] for tag in hashtags]),
         )
-
-        
-
-    #with Session() as session:
-    #    select_message_id = select(TelegramMessage.message_id)
-    #    message_id = session.execute(select_message_id).first()
-    #    insert_message_result = []
-    #    print(message_id)
-    #    if not message_id:
-    #        insert_message = TelegramMessage(message_id=sent_message.message_id)
-    #        insert_message_result.append(insert_message)
-    #        print(insert_message)
-    #        print(insert_message_result)
-    #        session.add(insert_message)
-    #        session.commit()#
-
-    #    await bot.pin_chat_message(
-    #        chat_id=message.chat.id,
-    #        message_id=message_id[0],
-    #        disable_notification=True,
-    #    )
-    #    print(insert_message_result)
-    #    print(message_id)
-    #    await bot.edit_message_text(
-    #        chat_id=message.chat.id,
-    #        message_id=message_id[0],
-    #        text=', '.join(hashtag_list_message),
-    #    )
-    #   print(message_id[0])
-
 
 
 def start_bot() -> None:
