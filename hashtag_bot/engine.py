cfb5f63aea91fc1ec1fdaa95efeaa0fb6347ca59
TurtleOld <dev@pavlovteam.ru>
2023-12-21 22:52:11 +0300

    refactoring

--- hashtag_bot/engine.py
+++ hashtag_bot/engine.py
@@ -1,10 +1,13 @@
 import os
 import asyncio
 
+from hashtag_bot.config.logger import logger
 from hashtag_bot.config.create_database import create_database
 from hashtag_bot.models.database import DATABASE_NAME
 from hashtag_bot.telegram_bot.hello import start_bot
 
+
+@logger.catch
 def main():
     db_is_created = os.path.exists(DATABASE_NAME)
     if not db_is_created:
@@ -12,6 +15,7 @@ def main():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(start_bot())
     loop.close()
-    
+
+
 if __name__ == '__main__':
     main()
