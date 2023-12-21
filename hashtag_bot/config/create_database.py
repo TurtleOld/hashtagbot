cb819948b3d78f85639365695173578c1f95a3ad
TurtleOld <dev@pavlovteam.ru>
2023-12-21 17:43:26 +0300

    Create HashTag Table

--- hashtag_bot/config/create_database.py
+++ hashtag_bot/config/create_database.py
@@ -1,5 +1,6 @@
 from hashtag_bot.models.database import create_db
 from hashtag_bot.models.telegram_message import TelegramMessage
+from hashtag_bot.models.hashtag_list import HashTag
 
 
 def create_database():
