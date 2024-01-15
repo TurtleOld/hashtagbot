1c135169ddd43c0922b59c7746ed8e270c278f17
TurtleOld <dev@pavlovteam.ru>
2024-01-15 11:41:25 +0300

    refactoring category add

--- hashtag_bot/telegram_bot/add_group.py
+++ hashtag_bot/telegram_bot/add_group.py
@@ -1,37 +1,37 @@
-from icecream import ic
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from telebot import types
 
 from hashtag_bot.config.bot import bot
 from hashtag_bot.database.database import DATABASE_URL
-from hashtag_bot.models.telegram import HashTag, CategoryHashTag
+from hashtag_bot.models.telegram import CategoryHashTag
 from hashtag_bot.telegram_bot.get_db_telegram_info import (
     get_telegram_chat,
     get_telegram_message,
-    get_hashtag,
 )
 
 
 async def add_hashtag_group(message: types.Message):
-    list_hashtag = message.text.split()[2:]
-    group_name = message.text.split()[1]
+    category_name = [word for word in message.text.split('"')][1]
     engine = create_async_engine(DATABASE_URL)
     async_session = async_sessionmaker(engine, expire_on_commit=False)
     async with async_session() as session:
         telegram_chat = await get_telegram_chat(session, message)
         telegram_message = await get_telegram_message(session, telegram_chat.id)
-        for hashtag in list_hashtag:
-            result = await session.execute(
-                select(HashTag).filter_by(
-                    message_id=telegram_message.id,
-                    name=hashtag,
-                )
+        category = await session.execute(
+            select(CategoryHashTag).filter_by(
+                name=category_name,
+                message_id=telegram_message.id,
             )
+        )
+        if not category.scalar_one_or_none():
             category_hashtag = CategoryHashTag(
-                name=group_name,
-                hashtag_id=result.scalar_one_or_none().id,
+                name=category_name,
                 message_id=telegram_message.id,
             )
             session.add(category_hashtag)
             await session.commit()
+        else:
+            await bot.send_message(
+                message.chat.id, f'Category "{category_name}" already exists'
+            )
