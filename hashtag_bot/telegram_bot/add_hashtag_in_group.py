dab9ce433ee53b88d89164d6eb8383b6c25c90a5
TurtleOld <dev@pavlovteam.ru>
2024-01-16 12:50:47 +0300

    released adding hashtags in category

--- hashtag_bot/telegram_bot/add_hashtag_in_group.py
+++ hashtag_bot/telegram_bot/add_hashtag_in_group.py
@@ -1,2 +1,44 @@
-async def add_hashtag_group():
-    ...
+import re
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from telebot import types
+
+from hashtag_bot.database.database import DATABASE_URL
+from hashtag_bot.models.telegram import CategoryHashTag, HashTag
+from hashtag_bot.telegram_bot.get_db_telegram_info import (
+    get_telegram_chat,
+    get_telegram_message,
+)
+
+
+async def add_hashtag_group(message: types.Message):
+    input_user = message.text.split('"')
+    category_name = input_user[1]
+    hashtags = [hashtag.strip() for hashtag in input_user[2:]][0].split()
+    print(category_name)
+    print(hashtags)
+    engine = create_async_engine(DATABASE_URL)
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+    async with async_session() as session:
+        telegram_chat = await get_telegram_chat(session, message)
+        telegram_message = await get_telegram_message(session, telegram_chat.id)
+        category_queryset = await session.execute(
+            select(CategoryHashTag).filter_by(
+                name=category_name,
+                message_id=telegram_message.id,
+            )
+        )
+        category = category_queryset.scalars().one()
+
+        if category.name:
+            for hashtag_item in hashtags:
+                hashtag_queryset = await session.execute(
+                    select(HashTag).filter_by(
+                        name=hashtag_item,
+                        message_id=telegram_message.id,
+                    )
+                )
+                result_hashtag = hashtag_queryset.scalars().all()
+                result_hashtag[0].category_id = category.id
+                await session.commit()
